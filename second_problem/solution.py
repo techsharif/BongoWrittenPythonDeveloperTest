28b285301a12a412b60930b2a6a19b870beda3f2
techsharif <sharif.cse.hstu@gmail.com>
2019-06-05 18:01:37 +0600

    solve second problem

--- second_problem/solution.py
+++ second_problem/solution.py
@@ -1,13 +1,30 @@
 def is_non_primitive(obj):
-    return True
+    return hasattr(obj, '__dict__')
 
 def calculate_depth(data, depth):
-	return []
+    if not data:
+        return []
+
+    if is_non_primitive(data):
+        return calculate_depth(data.__dict__, depth)
+
+    if type(data) == dict and len(data)==1:
+        key = list(data.keys())[0]
+        value = data[key]
+        if type(value) == dict or is_non_primitive(value):
+            return [[key, depth]] + calculate_depth(value, depth+1)
+        else:
+            return [[key, depth]]
+    else:
+        first_key = list(data.keys())[0]
+        first_value = data.pop(first_key)
+        first_data = {first_key:first_value}
+        return calculate_depth(first_data, depth) + calculate_depth(data, depth)
 
 
 def print_depth(data):
-	result_data = calculate_depth(data, 1)
-	print('\n'.join([ f'{datam[0]} {datam[1]}' for datam in result_data]))
+    result_data = calculate_depth(data, 1)
+    print('\n'.join([ f'{datam[0]} {datam[1]}' for datam in result_data]))
 
 if __name__ == '__main__':
     data = {
